f466fde9f4ec2aad3eb6186c4f0294e80f7f3f6b
CampusX <53361867+campusx-official@users.noreply.github.com>
2021-09-15 17:42:53 +0530

    Add files via upload

---
+++ feature_extractor.py
@@ -0,0 +1,47 @@
+# !pip install mtcnn==0.1.0
+# !pip install tensorflow==2.3.1
+# !pip install keras==2.4.3
+# !pip install keras-vggface==0.6
+# !pip install keras_applications==1.0.8
+
+#import os
+#import pickle
+
+#actors = os.listdir('data')
+
+#filenames = []
+
+#for actor in actors:
+    #for file in os.listdir(os.path.join('data',actor)):
+        #filenames.append(os.path.join('data',actor,file))
+
+#pickle.dump(filenames,open('filenames.pkl','wb'))
+
+from tensorflow.keras.preprocessing import image
+from keras_vggface.utils import preprocess_input
+from keras_vggface.vggface import VGGFace
+import numpy as np
+import pickle
+from tqdm import tqdm
+
+filenames = pickle.load(open('filenames.pkl','rb'))
+
+model = VGGFace(model='resnet50',include_top=False,input_shape=(224,224,3),pooling='avg')
+
+def feature_extractor(img_path,model):
+    img = image.load_img(img_path,target_size=(224,224))
+    img_array = image.img_to_array(img)
+    expanded_img = np.expand_dims(img_array,axis=0)
+    preprocessed_img = preprocess_input(expanded_img)
+
+    result = model.predict(preprocessed_img).flatten()
+
+    return result
+
+features = []
+
+for file in tqdm(filenames):
+    features.append(feature_extractor(file,model))
+
+pickle.dump(features,open('embedding.pkl','wb'))
+
---
+++ test.py
@@ -0,0 +1,47 @@
+from keras_vggface.utils import preprocess_input
+from keras_vggface.vggface import VGGFace
+import numpy as np
+import pickle
+from sklearn.metrics.pairwise import cosine_similarity
+import cv2
+from mtcnn import MTCNN
+from PIL import Image
+
+feature_list = np.array(pickle.load(open('embedding.pkl','rb')))
+filenames = pickle.load(open('filenames.pkl','rb'))
+
+model = VGGFace(model='resnet50',include_top=False,input_shape=(224,224,3),pooling='avg')
+
+detector = MTCNN()
+# load img -> face detection
+sample_img = cv2.imread('sample/satya.jpg')
+results = detector.detect_faces(sample_img)
+
+x,y,width,height = results[0]['box']
+
+face = sample_img[y:y+height,x:x+width]
+
+#  extract its features
+image = Image.fromarray(face)
+image = image.resize((224,224))
+
+face_array = np.asarray(image)
+
+face_array = face_array.astype('float32')
+
+expanded_img = np.expand_dims(face_array,axis=0)
+preprocessed_img = preprocess_input(expanded_img)
+result = model.predict(preprocessed_img).flatten()
+#print(result)
+#print(result.shape)
+# find the cosine distance of current image with all the 8655 features
+similarity = []
+for i in range(len(feature_list)):
+    similarity.append(cosine_similarity(result.reshape(1,-1),feature_list[i].reshape(1,-1))[0][0])
+
+index_pos = sorted(list(enumerate(similarity)),reverse=True,key=lambda x:x[1])[0][0]
+
+temp_img = cv2.imread(filenames[index_pos])
+cv2.imshow('output',temp_img)
+cv2.waitKey(0)
+# recommend that image
